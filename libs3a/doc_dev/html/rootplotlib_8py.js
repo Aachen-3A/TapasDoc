4fe4b7708c2238b5b0172ff66105c1010a2fd07b
Soren <soren.erdweg@cern.ch>
2015-02-11 13:48:37 +0100

    updated doxygen documentation for branch: dev

--- libs3a/doc_dev/html/rootplotlib_8py.js
+++ libs3a/doc_dev/html/rootplotlib_8py.js
@@ -4,5 +4,7 @@ var rootplotlib_8py =
     [ "Legend", "classrootplotlib_1_1Legend.html", "classrootplotlib_1_1Legend" ],
     [ "init", "rootplotlib_8py.html#ac9c9fd5886d49f4ad15eedaa8af18b10", null ],
     [ "Latex", "rootplotlib_8py.html#a782500c9d655a2faa883fe3a3f8a469c", null ],
-    [ "tdrStyle", "rootplotlib_8py.html#aaef7012494f2b1cfa4dad53b1733eb60", null ]
+    [ "pvaluetozvalue", "rootplotlib_8py.html#afad57b24c6123b9f707cfcf0b4bf63b5", null ],
+    [ "tdrStyle", "rootplotlib_8py.html#aaef7012494f2b1cfa4dad53b1733eb60", null ],
+    [ "zvaluetopvalue", "rootplotlib_8py.html#ac7aa77b80485154aff93a12283304ab2", null ]
 ];
