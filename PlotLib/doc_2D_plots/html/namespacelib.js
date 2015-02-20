9d5f685c1bbf7b9dec70681d8fa18661c2a0dbc9
Soren <soren.erdweg@cern.ch>
2015-02-20 13:44:26 +0100

    updated doxygen documentation for branch: dev

---
+++ PlotLib/doc_2D_plots/html/namespacelib.js
@@ -0,0 +1,11 @@
+var namespacelib =
+[
+    [ "configobj", "namespacelib_1_1configobj.html", "namespacelib_1_1configobj" ],
+    [ "DukePlotALot", "namespacelib_1_1DukePlotALot.html", "namespacelib_1_1DukePlotALot" ],
+    [ "DukePlotALot2D", "namespacelib_1_1DukePlotALot2D.html", "namespacelib_1_1DukePlotALot2D" ],
+    [ "ordered", "namespacelib_1_1ordered.html", "namespacelib_1_1ordered" ],
+    [ "plotlib", "namespacelib_1_1plotlib.html", "namespacelib_1_1plotlib" ],
+    [ "rootplotlib", "namespacelib_1_1rootplotlib.html", "namespacelib_1_1rootplotlib" ],
+    [ "rounding", "namespacelib_1_1rounding.html", "namespacelib_1_1rounding" ],
+    [ "style_class", "namespacelib_1_1style__class.html", "namespacelib_1_1style__class" ]
+];
---
+++ PlotLib/doc_2D_plots/html/rootplotlib_8py.js
@@ -0,0 +1,8 @@
+var rootplotlib_8py =
+[
+    [ "CmsDecoration", "classlib_1_1rootplotlib_1_1CmsDecoration.html", "classlib_1_1rootplotlib_1_1CmsDecoration" ],
+    [ "Legend", "classlib_1_1rootplotlib_1_1Legend.html", "classlib_1_1rootplotlib_1_1Legend" ],
+    [ "init", "rootplotlib_8py.html#afda153639aad556e63e5339eee8fb5e1", null ],
+    [ "Latex", "rootplotlib_8py.html#a1b0e3bb226edbcab75daaabdbd7f6d68", null ],
+    [ "tdrStyle", "rootplotlib_8py.html#a472cc4af506ea08fb89482a72ae28328", null ]
+];
